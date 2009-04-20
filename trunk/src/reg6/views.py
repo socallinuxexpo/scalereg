c13fe78ed33a5d4e44bcac7412788c2f219209c1
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-04-20 08:21:54 +0000

    reg6: Add model managers so we can do more complicated queries.

--- trunk/src/reg6/views.py
+++ trunk/src/reg6/views.py
@@ -7,25 +7,17 @@ import models
 STEPS_TOTAL = 5
 
 def index(request):
-  avail_tickets = []
-  avail_promocodes = {}
-
-  tickets = models.Ticket.objects.filter(public=True).order_by('description')
-  for t in tickets:
-    if t.is_public():
-      avail_tickets.append(t)
-  promocodes = models.PromoCode.objects.filter(active=True)
-  for p in promocodes:
-    if p.is_active():
-      avail_promocodes[p.name] = p
+  avail_tickets = models.Ticket.public_objects.order_by('description')
+  active_promocode_set = models.PromoCode.active_objects
+  avail_promocodes = active_promocode_set.names()
 
   promo_in_use = None
   if request.method == 'GET':
     if 'promo' in request.GET and request.GET['promo'] in avail_promocodes:
-      promo_in_use = avail_promocodes[request.GET['promo']]
+      promo_in_use = active_promocode_set.get(name=request.GET['promo'])
   elif request.method == 'POST':
     if 'promo' in request.POST and request.POST['promo'] in avail_promocodes:
-      promo_in_use = avail_promocodes[request.POST['promo']]
+      promo_in_use = active_promocode_set.get(name=request.POST['promo'])
 
   if promo_in_use:
     promo_name = promo_in_use.name
@@ -58,7 +50,7 @@ def AddItems(request):
         })
 
   return render_to_response('reg6/reg_items.html',
-    {'title': 'Blar',
+    {'title': 'Add Items',
      'step': 2,
      'steps_total': STEPS_TOTAL,
     })
