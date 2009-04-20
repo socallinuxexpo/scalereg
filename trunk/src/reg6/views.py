52b29839e5cf649d0fb166f8376701087a4fb587
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-04-20 08:22:05 +0000

    reg6: Add items page.

--- trunk/src/reg6/views.py
+++ trunk/src/reg6/views.py
@@ -6,6 +6,27 @@ import models
 
 STEPS_TOTAL = 5
 
+def ApplyPromoToTickets(promo, tickets):
+  if not promo:
+    return None
+  promo_name = promo.name
+  promo_applies_to = promo.applies_to.all()
+  for t in tickets:
+    if t in promo_applies_to:
+      t.price *= promo.price_modifier
+  return promo.name
+
+
+def ApplyPromoToItems(promo, items):
+  if not promo:
+    return None
+  promo_name = promo.name
+  for item in items:
+    if item.promo:
+      item.price *= promo.price_modifier
+  return promo.name
+
+
 def index(request):
   avail_tickets = models.Ticket.public_objects.order_by('description')
   active_promocode_set = models.PromoCode.active_objects
@@ -19,14 +40,7 @@ def index(request):
     if 'promo' in request.POST and request.POST['promo'] in avail_promocodes:
       promo_in_use = active_promocode_set.get(name=request.POST['promo'])
 
-  if promo_in_use:
-    promo_name = promo_in_use.name
-    promo_applies_to = promo_in_use.applies_to.all()
-    for t in avail_tickets:
-      if t in promo_applies_to:
-        t.price *= promo_in_use.price_modifier
-  else:
-    promo_name = None
+  promo_name = ApplyPromoToTickets(promo_in_use, avail_tickets)
 
   return render_to_response('reg6/reg_index.html',
     {'title': 'Registration',
@@ -40,6 +54,8 @@ def index(request):
 def AddItems(request):
   if request.method != 'POST':
     return HttpResponseRedirect('/reg6/')
+  if '/reg6/' not in request.META['HTTP_REFERER']:
+    return HttpResponseRedirect('/reg6/')
 
   required_vars = ['ticket', 'promo']
   for var in required_vars:
@@ -49,8 +65,23 @@ def AddItems(request):
          'error_message': 'No %s information.' % var,
         })
 
+  ticket = models.Ticket.public_objects.filter(name=request.POST['ticket'])
+  active_promocode_set = models.PromoCode.active_objects
+  avail_promocodes = active_promocode_set.names()
+  
+  promo_in_use = None
+  if request.POST['promo'] in avail_promocodes:
+    promo_in_use = active_promocode_set.get(name=request.POST['promo'])
+
+  promo_name = ApplyPromoToTickets(promo_in_use, ticket)
+  items = ticket[0].item_set.all().order_by('description')
+  ApplyPromoToItems(promo_in_use, items)
+
   return render_to_response('reg6/reg_items.html',
     {'title': 'Add Items',
+     'ticket': ticket[0],
+     'promo': promo_name,
+     'items': items,
      'step': 2,
      'steps_total': STEPS_TOTAL,
     })
