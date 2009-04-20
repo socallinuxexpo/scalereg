139bae7f635209ffb4d78bf1803923add8a56e20
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-04-20 16:06:43 +0000

    reports: Add report filters for booleans.

--- src/reports/views.py
+++ src/reports/views.py
@@ -1,6 +1,7 @@
 # Create your views here.
 
 from django.contrib.auth.decorators import login_required
+from django.db.models import BooleanField
 from django.db.models.base import ModelBase
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
@@ -10,6 +11,32 @@ from scale.auth_helper.models import Service
 from scale.reg6 import models
 import inspect
 import re
+import string
+
+class Filter:
+  def __init__(self, name):
+    self.name = name
+    self.items = {}
+    self.selected = -1
+
+  def get_items(self):
+    items = self.items.items()
+    items.sort()
+    return [v[1] for v in items]
+
+
+class Item:
+  def __init__(self, name, value):
+    self.name = name
+    self.value = value
+
+
+def paranoid_strip(value):
+  valid_chars = string.ascii_letters + string.digits + '_'
+  for c in value:
+    if c not in valid_chars:
+      raise ValueError
+  return value
 
 @login_required
 def index(request):
@@ -60,6 +87,8 @@ def object_list(request, queryset, paginate_by=None, page=None,
   allow_empty=False, template_name=None, template_loader=loader,
   extra_context=None, context_processors=None, template_object_name='object',
   mimetype=None):
+  all_fields = [f.name for f in queryset.model._meta.fields]
+
   if not extra_context:
     extra_context = {}
 
@@ -67,10 +96,41 @@ def object_list(request, queryset, paginate_by=None, page=None,
     extra_context['title'] = queryset.model._meta.verbose_name_plural.title()
 
   if 'field_list' not in extra_context:
-    extra_context['field_list'] = [f.name for f in queryset.model._meta.fields]
+    extra_context['field_list'] = all_fields
+
+  filter_select = {}
+  for i in xrange(len(all_fields)):
+    name = all_fields[i]
+    filter = Filter(name)
+    field_type = type(queryset.model._meta.fields[i])
+    if field_type == BooleanField:
+      filter.items[-1] = (Item('All', -1))
+      filter.items[0] = (Item('False', 0))
+      filter.items[1] = (Item('True', 1))
+    else:
+      continue
+    filter_select[name] = filter
+
+  for f in request.GET:
+    if not f.startswith('filter__'):
+      continue
+    name = f[8:]
+    field_type = type(queryset.model._meta.fields[all_fields.index(name)])
+    if name and name in filter_select:
+      if field_type == BooleanField:
+        filter = filter_select[name]
+        try:
+          value = int(request.GET[f])
+        except ValueError:
+          continue
+        if value in filter.items and value != -1:
+          filter.selected = value
+          query_string = '%s = %%s' % paranoid_strip(name)
+          queryset = queryset.extra(where=[query_string], params=[value])
+  extra_context['filter_select'] = filter_select.values()
 
   if 'order' in request.GET:
-    if request.GET['order'] in [f.name for f in queryset.model._meta.fields]:
+    if request.GET['order'] in all_fields:
       ordering = request.GET['order']
       extra_context['order'] = ordering
       if 'dec' in request.GET:
