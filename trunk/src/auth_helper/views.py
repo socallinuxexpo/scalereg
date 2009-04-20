4c765eb2dedc6cf387b85e261aae1c86d43a9d5c
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-04-20 06:50:09 +0000

    accounts: Implement auth_helper.

--- trunk/src/auth_helper/views.py
+++ trunk/src/auth_helper/views.py
@@ -1,1 +1,18 @@
 # Create your views here.
+
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import Context, loader
+from scale.auth_helper.models import Service
+
+def index(request):
+  return HttpResponseRedirect('/accounts/profile/')
+
+@login_required
+def profile(request):
+  services = Service.objects.filter(users=request.user)
+  t = loader.get_template('profile/index.html')
+  c = Context({'user': request.user, 'title': 'Available Services', 'services': services})
+  return HttpResponse(t.render(c))
+
+
