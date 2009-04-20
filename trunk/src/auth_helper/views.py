48f49864c52f5ebcd42b0482b186ccf48d4000cb
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-04-20 08:29:09 +0000

    reports: Superuser should be able to view all services.

--- trunk/src/auth_helper/views.py
+++ trunk/src/auth_helper/views.py
@@ -11,12 +11,15 @@ def index(request):
 @login_required
 def profile(request):
   # figure out what servers are available
-  services_user = Service.objects.filter(users=request.user)
-  services_group = None
-  for f in request.user.groups.all():
-    services_group = services_group or Service.objects.filter(groups=f)
-  services = services_group or services_user
-  services = services.filter(active=True).order_by('name')
+  if request.user.is_superuser:
+    services = Service.objects.all()
+  else:
+    services_user = Service.objects.filter(users=request.user)
+    services_group = None
+    for f in request.user.groups.all():
+      services_group = services_group or Service.objects.filter(groups=f)
+    services = services_group or services_user
+    services = services.filter(active=True).order_by('name')
 
   return render_to_response('profile/index.html',
     {'user': request.user, 'title': 'Available Services', 'services': services})
