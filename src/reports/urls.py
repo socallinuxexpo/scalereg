6a014c4447c1570c63cd2555a5c9e7da829620ae
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-04-20 16:07:38 +0000

    reports: Add totals and attendee figures to reports.

--- src/reports/urls.py
+++ src/reports/urls.py
@@ -3,6 +3,10 @@ from scale.reg6 import models
 
 attendee_dict = {
     'queryset': models.Attendee.objects.all(),
+    'extra_context': {
+    'valid': len(models.Attendee.objects.all().filter(valid=True)),
+    'checkin': len(models.Attendee.objects.all().filter(checked_in=True)),
+    },
     'allow_empty': True,
 }
 
