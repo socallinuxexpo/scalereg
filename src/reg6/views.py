b09eb5e1c587a4a15a3eded8c980143c6bd887be
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-04-20 08:15:55 +0000

    reg6: Registration tickets page should handle POST as well.

--- src/reg6/views.py
+++ src/reg6/views.py
@@ -4,33 +4,38 @@ from django.shortcuts import render_to_response
 import models
 
 def index(request):
-  available_tickets = []
-  available_promocodes = {}
-  promo = False
+  avail_tickets = []
+  avail_promocodes = {}
 
   tickets = models.Ticket.objects.filter(public=True).order_by('description')
   for t in tickets:
     if t.is_public():
-      available_tickets.append(t)
+      avail_tickets.append(t)
   promocodes = models.PromoCode.objects.filter(active=True)
   for p in promocodes:
     if p.is_active():
-      available_promocodes[p.name] = p
+      avail_promocodes[p.name] = p
 
-  if 'promo' in request.GET and request.GET['promo'] in available_promocodes:
-    promo_in_use = available_promocodes[request.GET['promo']]
-    promo = promo_in_use.name
+  promo_in_use = None
+  if request.method == 'GET':
+    if 'promo' in request.GET and request.GET['promo'] in avail_promocodes:
+      promo_in_use = avail_promocodes[request.GET['promo']]
+  elif request.method == 'POST':
+    if 'promo' in request.POST and request.POST['promo'] in avail_promocodes:
+      promo_in_use = avail_promocodes[request.POST['promo']]
+
+  if promo_in_use:
     promo_applies_to = promo_in_use.applies_to.all()
-    for t in available_tickets:
+    for t in avail_tickets:
       if t in promo_applies_to:
         t.price *= promo_in_use.price_modifier
 
   return render_to_response('reg6/reg_index.html',
-    {'title': 'Registration', 'tickets': available_tickets,
-     'promo': promo})
+    {'title': 'Registration', 'tickets': avail_tickets,
+     'promo': promo_in_use.name})
 
 def AddItems(request):
-  return render_to_response('reg6/index.html',
+  return render_to_response('reg6/reg_items.html',
     {'title': 'Blar'})
 
 def AddAttendee(request):
