0f5d5539f846a7d1b8711dd53a364a31fdfb2421
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-04-20 06:48:00 +0000

    Make prices floats, finish promocode, add __str__().

--- src/reg6/models.py
+++ src/reg6/models.py
@@ -11,7 +11,7 @@ SALUTATION_CHOICES = (
 
 class Order(models.Model):
   # basic info
-  date = models.DateTimeField()
+  date = models.DateTimeField(auto_now_add=True)
   order_num = models.CharField(maxlength=10, primary_key=True)
   valid = models.BooleanField()
 
@@ -28,17 +28,23 @@ class Order(models.Model):
   phone = models.CharField(maxlength=20, blank=True)
 
   # payment info
-  amount = models.PositiveIntegerField()
+  amount = models.FloatField(max_digits=4, decimal_places=2)
   payment_type = models.CharField(maxlength=10)
   auth_code = models.CharField(maxlength=30)
   resp_msg = models.CharField(maxlength=60)
   result = models.CharField(maxlength=60)
 
+  def __str__(self):
+    return "%s (%s)" % (self.name, self.order_num)
+
 
 class Ticket(models.Model):
   code = models.CharField(maxlength=1, primary_key=True)
   description = models.CharField(maxlength=60)
-  price = models.PositiveIntegerField()
+  price = models.FloatField(max_digits=5, decimal_places=2)
+
+  def __str__(self):
+    return self.description
 
 
 class Attendee(models.Model):
@@ -65,14 +71,32 @@ class Attendee(models.Model):
   survey_answers = models.CharField(maxlength=60)
   order = models.ForeignKey(Order)
 
+  def __str__(self):
+    return "%s %s (%s)" % (self.first_name, self.last_name, self.badge_id)
+
 
 class Item(models.Model):
   name = models.CharField(maxlength=4)
   description = models.CharField(maxlength=60)
-  price = models.PositiveIntegerField()
+
+  price = models.FloatField(max_digits=5, decimal_places=2)
+
   active = models.BooleanField()
   pickup = models.BooleanField()
+
+  def __str__(self):
+    return self.description
 
 
 class PromoCode(models.Model):
   name = models.CharField(maxlength=5)
+  description = models.CharField(maxlength=60)
+
+  price_modifier = models.FloatField(max_digits=3, decimal_places=2)
+
+  active = models.BooleanField()
+  start_date = models.DateField()
+  expiration_date = models.DateField()
+
+  def __str__(self):
+    return self.description
