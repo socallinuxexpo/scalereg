358c75ec2a2ade4c53f4bb6099fbd1e7bf5f2560
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-04-20 08:28:40 +0000

    reg6: Add registration finish page.

--- src/reg6/urls.py
+++ src/reg6/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
     (r'^$', 'scale.reg6.views.index'),
     (r'^add_items/$', 'scale.reg6.views.AddItems'),
     (r'^add_attendee/$', 'scale.reg6.views.AddAttendee'),
+    (r'^finish_registration/$', 'scale.reg6.views.FinishRegistration'),
 )
