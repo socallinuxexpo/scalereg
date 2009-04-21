a0ef042a9a4489b588ff1fe14048b18c302f5109
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-04-21 06:18:17 +0000

    reg6: Add staff check in page.

--- src/reg6/staff/views.py
+++ src/reg6/staff/views.py
@@ -3,6 +3,7 @@
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
+from scale.reg6 import models
 from scale.reports.views import reports_perm_checker
 
 @login_required
@@ -14,3 +15,60 @@ def index(request):
     {'title': 'Staff Page',
     })
 
+@login_required
+def CheckIn(request):
+  can_access = reports_perm_checker(request.user, request.path)
+  if not can_access:
+    return HttpResponseRedirect('/accounts/profile/')
+
+  if request.method == 'GET':
+    return render_to_response('reg6/staff/checkin.html',
+      {'title': 'Attendee Check In',
+      })
+
+  attendees = []
+  if request.POST['last_name']:
+    attendees = models.Attendee.objects.filter(valid=True, checked_in=False,
+      last_name__icontains=request.POST['last_name'])
+  if not attendees:
+    attendees = models.Attendee.objects.filter(valid=True, checked_in=False,
+      zip=request.POST['zip'])
+
+  return render_to_response('reg6/staff/checkin.html',
+    {'title': 'Attendee Check In',
+     'attendees': attendees,
+     'last': request.POST['last_name'],
+     'zip': request.POST['zip'],
+     'search': 1,
+    })
+
+@login_required
+def FinishCheckIn(request):
+  can_access = reports_perm_checker(request.user, request.path)
+  if not can_access:
+    return HttpResponseRedirect('/accounts/profile/')
+
+  if request.method != 'POST':
+    return HttpResponseRedirect('/reg6/')
+
+  if 'id' not in request.POST:
+    return render_to_response('error.html',
+      {'error_message': 'No ID'})
+
+  try:
+    attendee = models.Attendee.objects.get(id=request.POST['id'])
+  except models.Attendee.DoesNotExist:
+    return render_to_response('error.html',
+      {'error_message': 'We could not find your registration'})
+
+  try:
+    attendee.checked_in = True
+    attendee.save()
+  except:
+    return render_to_response('error.html',
+      {'error_message': 'We encountered a problem with your checkin'})
+
+  return render_to_response('reg6/staff/finish_checkin.html',
+    {'title': 'Attendee Check In',
+     'attendee': attendee,
+    })
