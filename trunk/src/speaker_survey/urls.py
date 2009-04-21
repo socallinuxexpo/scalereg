ab8c9d175b088fed3307f33334557670a684dbb4
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-04-21 07:11:53 +0000

    speaker_survey: Add scoring pages.

--- trunk/src/speaker_survey/urls.py
+++ trunk/src/speaker_survey/urls.py
@@ -4,4 +4,6 @@ urlpatterns = patterns('',
     (r'^$', 'scale.speaker_survey.views.SurveyLookup'),
     (r'^mass_add/$', 'scale.speaker_survey.views.MassAdd'),
     (r'^url_dump/$', 'scale.speaker_survey.views.UrlDump'),
+    (r'^scores/$', 'scale.speaker_survey.views.Scores'),
+    (r'^scores/(?P<id>\d{1,4})/$', 'scale.speaker_survey.views.Scores'),
 )
--- trunk/src/speaker_survey/views.py
+++ trunk/src/speaker_survey/views.py
@@ -104,3 +104,110 @@ def UrlDump(request):
     response.write('%s\n' % f.email)
     response.write('%s\n' % hash)
   return response
+
+
+def GetScore(score_choice):
+  if score_choice == '0sd':
+    return -2
+  elif score_choice == '1di':
+    return -1
+  elif score_choice == '2ne':
+    return 0
+  elif score_choice == '3ag':
+    return 1
+  elif score_choice == '4sa':
+    return 2
+  else:
+    raise ValueError(score_choice)
+
+
+def ScoreTotal(speakers):
+  scores = []
+  for sp in speakers:
+    surveys = models.Survey7X.objects.filter(speaker=sp)
+    num = surveys.count()
+    if num == 0:
+      sp.score1 = 'N/A'
+      sp.score2 = 'N/A'
+      sp.num = 0
+      continue
+
+    sc1 = 0
+    sc2 = 0
+    for sv in surveys:
+      sc1 = sum( [ GetScore(getattr(sv, 'q%02d' % i)) for i in xrange(0, 7) ] )
+      sc2 = sum( [ GetScore(getattr(sv, 'q%02d' % i)) for i in xrange(7, 15) ] )
+    sp.score1 = round(float(sc1) / num, 1)
+    sp.score2 = round(float(sc2) / num, 1)
+    sp.num = num
+
+
+def ScoreSurveys(surveys):
+  scores = []
+  comments = []
+  for i in xrange(0, 15):
+    scores.append([0, 0, 0, 0, 0, 0])
+
+  num = surveys.count()
+
+  sc1 = 0
+  sc2 = 0
+  for sv in surveys:
+    if sv.comments:
+      comments.append(sv.comments)
+
+    for i in xrange(0, 7):
+      s = getattr(sv, 'q%02d' % i)
+      scores[i][int(s[0])] += 1
+      sc1 += GetScore(s)
+    for i in xrange(7, 15):
+      s = getattr(sv, 'q%02d' % i)
+      scores[i][int(s[0])] += 1
+      sc2 += GetScore(s)
+  for i in xrange(0, 15):
+    total = sum([ GetScore(models.VALUE_CHOICES[j][0]) * scores[i][j] for j in xrange(0,5) ])
+    scores[i][5] = round(float(total) / num, 1)
+  sc1 = round(float(sc1) / num, 1)
+  sc2 = round(float(sc2) / num, 1)
+  return (scores, sc1, sc2, num, comments)
+
+
+@login_required
+def Scores(request, id=None):
+  if not request.user.is_staff:
+    return HttpResponse('')
+  if not id:
+    speakers = models.Speaker.objects.all().order_by('name')
+    ScoreTotal(speakers)
+    return render_to_response('speaker_survey/scores_index.html',
+      {'title': 'Speaker Scores',
+       'speakers': speakers,
+      })
+
+  speaker = None
+  try:
+    speaker = models.Speaker.objects.get(id=id)
+  except models.Speaker.DoesNotExist:
+    return render_to_response('speaker_survey/error.html',
+      {'title': 'Survey Error',
+       'error_message': 'Cannot find speaker',
+      })
+  surveys = models.Survey7X.objects.filter(speaker=speaker)
+  if not surveys:
+    return render_to_response('speaker_survey/error.html',
+      {'title': 'Survey Error',
+       'error_message': 'Speaker has no filled survey response',
+      })
+
+  scores, sc1, sc2, num, comments = ScoreSurveys(surveys)
+  return render_to_response('speaker_survey/scores_speaker.html',
+    {'title': 'Speaker Scores',
+     'speaker': speaker,
+     'survey': surveys[0],
+     'scores': scores,
+     'sc1': sc1,
+     'sc2': sc2,
+     'num': num,
+     'comments': comments,
+     'choices': [ x[1] for x in models.VALUE_CHOICES ],
+    })
