31e44e89d088bfec56a0792224f080b518d67fec
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-04-21 07:11:20 +0000

    speaker_survey: Add a attendee lookup operation.

--- trunk/src/speaker_survey/views.py
+++ trunk/src/speaker_survey/views.py
@@ -1,6 +1,8 @@
 # Create your views here.
 
 from django.shortcuts import render_to_response
+from scale.reg6 import models as reg6models
+from scale.speaker_survey import validators
 
 def SurveyLookup(request):
   if request.method == 'GET':
@@ -9,7 +11,35 @@ def SurveyLookup(request):
        'unknown': False,
       })
   else:
-    return render_to_response('speaker_survey/error.html',
-      {'title': 'Survey Error',
-       'error_message': 'Invalid Operation',
-      })
+    error = False
+    hash = None
+    id = -1
+    if 'name' not in request.POST or 'id' not in request.POST:
+      error = True
+    if not error:
+      try:
+        id = int(request.POST['id'])
+      except ValueError:
+        error = True
+
+    if not error:
+      try:
+        attendee = reg6models.Attendee.objects.get(id=id)
+        if attendee.first_name == request.POST['name']:
+          hash = validator.hash(attendee.first_name + attendee.last_name)[:6]
+          hash += '%04d' % id
+      except reg6models.Attendee.DoesNotExist:
+        error = True
+
+    if hash:
+      return render_to_response('speaker_survey/survey_lookup.html',
+        {'title': 'Speaker Surveys',
+         'hash': hash,
+        })
+    else:
+      return render_to_response('speaker_survey/survey_lookup.html',
+        {'title': 'Speaker Surveys',
+         'id': request.POST['id'],
+         'name': request.POST['name'],
+         'unknown': True,
+        })
