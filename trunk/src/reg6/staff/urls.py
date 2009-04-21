6882b5898503efbc993552bcbd517c38da518fe3
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-04-21 06:18:17 +0000

    reg6: Add staff check in page.

--- trunk/src/reg6/staff/urls.py
+++ trunk/src/reg6/staff/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^$', 'scale.reg6.staff.views.index'),
+    (r'^checkin/$', 'scale.reg6.staff.views.CheckIn'),
+    (r'^finish_checkin/$', 'scale.reg6.staff.views.FinishCheckIn'),
 )
