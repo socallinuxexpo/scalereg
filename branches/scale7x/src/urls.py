edd035c39794f0af3a5d7770c16ec6d556794af4
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-05-10 22:19:23 +0000

    Add a branch for the Scale 7x code. AKA the Django 0.96 branch.

---
+++ branches/scale7x/src/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls.defaults import *
+
+urlpatterns = patterns('',
+    # Example:
+    # (r'^scale/', include('scale.apps.foo.urls.foo')),
+
+    (r'^accounts/$', 'scale.auth_helper.views.index'),
+    (r'^accounts/profile/$', 'scale.auth_helper.views.profile'),
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
+    (r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+    (r'^accounts/password_change/$', 'django.contrib.auth.views.password_change'),
+    (r'^accounts/password_change/done/$', 'django.contrib.auth.views.password_change_done'),
+    (r'^admin/', include('django.contrib.admin.urls')),
+    (r'^reg6/', include('scale.reg6.urls')),
+    (r'^reports/', include('scale.reports.urls')),
+    (r'^speaker_survey/', include('scale.speaker_survey.urls')),
+
+    # dummy index page
+    (r'^$', 'scale.auth_helper.views.index'),
+
+)
