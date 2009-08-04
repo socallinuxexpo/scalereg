320198be01e41b31d483a911de951e541b126aa9
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-08-04 08:44:14 +0000

    simple_cfp: Add speaker registration.

--- scalereg/simple_cfp/urls.py
+++ scalereg/simple_cfp/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^$', 'scalereg.simple_cfp.views.index'),
+    (r'^speaker_registration/$', 'scalereg.simple_cfp.views.RegisterSpeaker'),
 )
--- scalereg/simple_cfp/views.py
+++ scalereg/simple_cfp/views.py
@@ -1,4 +1,43 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from scalereg.simple_cfp import forms
+from scalereg.simple_cfp import models
+from scalereg.common import utils
+import datetime
+
+def GenerateSpeakerValidationCode():
+  speaker_ids = [s.validation_code for s in models.Speaker.objects.all()]
+  return utils.GenerateUniqueID(10, speaker_ids)
+
 
 def index(request):
   return HttpResponse('Ok')
+
+
+def RegisterSpeaker(request):
+  title = 'Register Speaker'
+
+  if request.method == 'POST':
+    form = forms.SpeakerForm(request.POST)
+    if not form.is_valid():
+      return render_to_response('simple_cfp/cfp_speaker.html',
+        {'title': title,
+         'form': form,
+        })
+
+    new_speaker = form.save(commit=False)
+    new_speaker.valid = False
+    new_speaker.validation_code = GenerateSpeakerValidationCode()
+    new_speaker.signup_date = datetime.datetime.now()
+    new_speaker = form.save()
+
+    return render_to_response('simple_cfp/cfp_speaker_registered.html',
+    {'title': title,
+     'speaker': new_speaker,
+    })
+
+  else:
+    return render_to_response('simple_cfp/cfp_speaker.html',
+      {'title': title,
+       'form': forms.SpeakerForm(),
+      })
