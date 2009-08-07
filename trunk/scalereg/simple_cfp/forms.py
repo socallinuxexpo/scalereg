d0b379cad5cbaaafb5904147d6780244ef5f6861
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-08-07 09:19:52 +0000

    simple_cfp: Add presentation submission form.

--- trunk/scalereg/simple_cfp/forms.py
+++ trunk/scalereg/simple_cfp/forms.py
@@ -1,5 +1,20 @@
 from django.forms import ModelForm
+from scalereg.simple_cfp.models import Presentation
 from scalereg.simple_cfp.models import Speaker
+
+
+class PresentationForm(ModelForm):
+  class Meta:
+    model = Presentation
+    fields = (
+      'speaker_code',
+      'categories',
+      'audiences',
+      'title',
+      'description',
+      'short_abstract',
+      'long_abstract',
+    )
 
 
 class SpeakerForm(ModelForm):
--- trunk/scalereg/simple_cfp/urls.py
+++ trunk/scalereg/simple_cfp/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     (r'^$', 'scalereg.simple_cfp.views.index'),
     (r'^speaker_registration/$', 'scalereg.simple_cfp.views.RegisterSpeaker'),
+    (r'^submit_paper/$', 'scalereg.simple_cfp.views.SubmitPaper'),
 )
