d33e3f0d4cd829a639e67d1b60d676f31de6e933
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-08-07 08:48:02 +0000

    reg6: Fix some formatting issues.

--- scalereg/reg6/admin.py
+++ scalereg/reg6/admin.py
@@ -8,29 +8,35 @@ from reg6.models import PromoCode
 from reg6.models import Question
 from reg6.models import Ticket
 
+
 class AnswerInline(admin.TabularInline):
   model = Answer
   extra = 3
 
+
 class PromoCodeOptions(admin.ModelAdmin):
   list_display = ('name', 'description', 'price_modifier', 'active',
-      'start_date', 'end_date')
+                  'start_date', 'end_date')
   list_filter = ('active', 'start_date', 'end_date')
   save_on_top = True
+
 
 class QuestionOptions(admin.ModelAdmin):
   save_on_top = True
   model = Question
   inlines = [AnswerInline]
 
+
 class ItemOptions(admin.ModelAdmin):
   list_display = ('name', 'description', 'price', 'active', 'pickup', 'promo')
   list_filter = ('active', 'pickup', 'promo')
   save_on_top = True
 
+
 class AnswerOptions(admin.ModelAdmin):
   list_display = ('question', '__str_text__')
   save_on_top = True
+
 
 class OrderOptions(admin.ModelAdmin):
   fieldsets = (
@@ -42,9 +48,11 @@ class OrderOptions(admin.ModelAdmin):
                                  'resp_msg', 'result')}),
   )
   list_display = ('order_num', 'date', 'name', 'address', 'city', 'state',
-      'zip', 'country', 'email', 'phone', 'amount', 'payment_type', 'valid')
+                  'zip', 'country', 'email', 'phone', 'amount', 'payment_type',
+                  'valid')
   list_filter = ('date', 'payment_type', 'valid')
   save_on_top = True
+
 
 class AttendeeOptions(admin.ModelAdmin):
   fieldsets = (
@@ -56,21 +64,24 @@ class AttendeeOptions(admin.ModelAdmin):
     ('Misc', {'fields': ('promo', 'order', 'answers')}),
   )
   list_display = ('id', 'first_name', 'last_name', 'email', 'zip',
-      'badge_type', 'valid', 'checked_in', 'order', 'promo')
+                  'badge_type', 'valid', 'checked_in', 'order', 'promo')
   list_filter = ('badge_type', 'valid', 'checked_in', 'promo')
   save_on_top = True
 
+
 class CouponOptions(admin.ModelAdmin):
   list_display = ('code', 'badge_type', 'order', 'used', 'max_attendees',
-      'expiration')
+                  'expiration')
   list_filter = ('code', 'used', 'badge_type')
   save_on_top = True
 
+
 class TicketOptions(admin.ModelAdmin):
   list_display = ('name', 'description', 'type', 'price', 'public',
-    'start_date', 'end_date')
+                  'start_date', 'end_date')
   list_filter = ('type', 'public', 'start_date', 'end_date')
   save_on_top = True
+
 
 admin.site.register(PromoCode, PromoCodeOptions)
 admin.site.register(Question, QuestionOptions)
@@ -80,4 +91,3 @@ admin.site.register(Order, OrderOptions)
 admin.site.register(Attendee, AttendeeOptions)
 admin.site.register(Coupon, CouponOptions)
 admin.site.register(Ticket, TicketOptions)
-
