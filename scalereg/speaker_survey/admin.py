677da4aff3a12d1a2ed5592ead9cd7a3a1367afe
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-08-07 08:47:54 +0000

    speaker_survey: Fix some formatting issues.

--- scalereg/speaker_survey/admin.py
+++ scalereg/speaker_survey/admin.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from speaker_survey.models import Speaker
 from speaker_survey.models import Survey7X
 
+
 class Survey7XOptions(admin.ModelAdmin):
   save_on_top = True
+
 
 class SpeakerOptions(admin.ModelAdmin):
   save_on_top = True
 
+
 admin.site.register(Survey7X, Survey7XOptions)
 admin.site.register(Speaker, SpeakerOptions)
-
