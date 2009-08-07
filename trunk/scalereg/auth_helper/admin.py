87adf7104a57135af14811347f797a570e5e1918
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-08-07 08:47:44 +0000

    auth_helper: Fix some formatting issues.

--- trunk/scalereg/auth_helper/admin.py
+++ trunk/scalereg/auth_helper/admin.py
@@ -1,8 +1,9 @@
 from auth_helper.models import Service
 from django.contrib import admin
 
+
 class ServiceOptions(admin.ModelAdmin):
   list_display = ('name', 'active', 'url')
 
-admin.site.register(Service, ServiceOptions)
 
+admin.site.register(Service, ServiceOptions)
