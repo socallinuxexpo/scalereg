09f8003fa1cacd16a830db286b43dec3bf43741f
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-08-07 09:02:28 +0000

    simple_cfp: Set some reasonable speaker model defaults.

--- scalereg/simple_cfp/views.py
+++ scalereg/simple_cfp/views.py
@@ -4,7 +4,6 @@ from django.shortcuts import render_to_response
 from scalereg.simple_cfp import forms
 from scalereg.simple_cfp import models
 from scalereg.common import utils
-import datetime
 
 if settings.USE_RECAPTCHA:
   from recaptcha.client.captcha import displayhtml
@@ -59,9 +58,7 @@ def RegisterSpeaker(request):
           })
 
     new_speaker = form.save(commit=False)
-    new_speaker.valid = True
     new_speaker.validation_code = GenerateSpeakerValidationCode()
-    new_speaker.signup_date = datetime.datetime.now()
     new_speaker = form.save()
 
     return render_to_response('simple_cfp/cfp_speaker_registered.html',
