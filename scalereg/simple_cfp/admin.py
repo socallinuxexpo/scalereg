872e35fbf525001b20ca2a1ea4dfbb822c440231
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-08-07 09:02:50 +0000

    simple_cfp: Better organize the speaker admin interface.

--- scalereg/simple_cfp/admin.py
+++ scalereg/simple_cfp/admin.py
@@ -1,4 +1,18 @@
 from django.contrib import admin
 from simple_cfp.models import Speaker
 
-admin.site.register(Speaker)
+
+class SpeakerOptions(admin.ModelAdmin):
+  fieldsets = (
+    ('Speaker Info', {'fields': ('salutation', 'first_name', 'last_name',
+                                 'title', 'org')}),
+    ('Contact Info', {'fields': ('email', 'zip', 'phone')}),
+    ('Other Info', {'fields': ('url', 'bio')}),
+    ('Validation Info', {'fields': ('valid', 'validation_code')}),
+  )
+  list_display = ('id', 'first_name', 'last_name', 'title', 'org', 'email',
+                  'valid', 'validation_code')
+  save_on_top = True
+
+
+admin.site.register(Speaker, SpeakerOptions)
