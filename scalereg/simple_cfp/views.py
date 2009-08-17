33004355c170498ab4353fbba0bb12c743127ff6
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-08-17 05:17:02 +0000

    simple_cfp: Implement feature to recover validation codes by email.

--- scalereg/simple_cfp/views.py
+++ scalereg/simple_cfp/views.py
@@ -6,6 +6,10 @@ from scalereg.simple_cfp import forms
 from scalereg.simple_cfp import models
 from scalereg.common import utils
 
+if settings.SCALEREG_SIMPLECFP_SEND_MAIL:
+  from django.core.mail import send_mail
+  import smtplib
+
 if settings.SCALEREG_SIMPLECFP_USE_RECAPTCHA:
   from recaptcha.client.captcha import displayhtml
   from recaptcha.client.captcha import submit
@@ -13,6 +17,7 @@ if settings.SCALEREG_SIMPLECFP_USE_RECAPTCHA:
 class ErrorMsg:
   # Common messages
   CAPTCHA_SERVER_ERROR = 'Could not contact reCAPTCHA server'
+  EMAIL_ERROR = 'Could not send email'
   # SubmitPresentation
   INVALID_CODE = 'Invalid speaker code'
   INVALID_EMAIL = 'Contact email does not match speaker code'
@@ -36,10 +41,83 @@ def GenerateSpeakerValidationCode():
   return utils.GenerateUniqueID(10, speaker_ids)
 
 
+def SendValidationEmail(speaker):
+  if not settings.SCALEREG_SIMPLECFP_SEND_MAIL:
+    return False
+  try:
+    send_mail("Your simple_cfp password",
+              "Your simple_cfp password is %s" % speaker.validation_code,
+              settings.SCALEREG_EMAIL,
+              [speaker.contact_email])
+    return True
+  except smtplib.SMTPException:
+    return False
+
+
 def index(request):
   return render_to_response('simple_cfp/cfp_index.html',
     {'title': 'Simple CFP',
+     'send_email': settings.SCALEREG_SIMPLECFP_SEND_MAIL,
     })
+
+
+def RecoverValidation(request):
+  TITLE = 'Recover Validation Code'
+
+  if not settings.SCALEREG_SIMPLECFP_SEND_MAIL:
+    return HttpResponse()
+
+  if request.method == 'POST':
+    if 'email' in request.POST:
+      email = request.POST['email']
+    else:
+      email = ''
+
+    if settings.SCALEREG_SIMPLECFP_USE_RECAPTCHA:
+      try:
+        recaptcha_response = submit(request.POST['recaptcha_challenge_field'],
+                                    request.POST['recaptcha_response_field'],
+                                    settings.RECAPTCHA_PRIVATE_KEY,
+                                    request.META['REMOTE_ADDR'])
+      except:
+        return render_to_response('simple_cfp/cfp_error.html',
+          {'title': TITLE,
+           'error_message': ErrorMsg.CAPTCHA_SERVER_ERROR,
+          })
+      if not recaptcha_response.is_valid:
+        recaptcha_html = GenerateRecaptchaHTML(request,
+                                               recaptcha_response.error_code)
+        return render_to_response('simple_cfp/cfp_recover_validation.html',
+          {'title': TITLE,
+           'email': email,
+           'recaptcha_html': recaptcha_html,
+          })
+
+    speakers = models.Speaker.objects.filter(contact_email=email)
+    speakers = speakers.filter(valid=True)
+    if not speakers:
+      return render_to_response('simple_cfp/cfp_recover_validation.html',
+        {'title': TITLE,
+         'email': email,
+         'error': True,
+         'recaptcha_html': GenerateRecaptchaHTML(request),
+        })
+
+    if not SendValidationEmail(speakers[0]):
+      return render_to_response('simple_cfp/cfp_error.html',
+        {'title': TITLE,
+         'error_message': ErrorMsg.EMAIL_ERROR,
+        })
+    return render_to_response('simple_cfp/cfp_recover_validation.html',
+      {'title': TITLE,
+       'email': email,
+       'sent': True,
+      })
+  else:
+    return render_to_response('simple_cfp/cfp_recover_validation.html',
+      {'title': TITLE,
+       'recaptcha_html': GenerateRecaptchaHTML(request),
+      })
 
 
 def RegisterSpeaker(request):
