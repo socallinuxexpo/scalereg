7f14855e249ccd42fad00f40b037b38311af1560
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-08-24 04:26:12 +0000

    simple_cfp: Add submission status page.

--- scalereg/simple_cfp/urls.py
+++ scalereg/simple_cfp/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('',
     (r'^$', 'scalereg.simple_cfp.views.index'),
     (r'^recover_validation/$', 'scalereg.simple_cfp.views.RecoverValidation'),
     (r'^speaker_registration/$', 'scalereg.simple_cfp.views.RegisterSpeaker'),
+    (r'^submission_status/$', 'scalereg.simple_cfp.views.SubmissionStatus'),
     (r'^submit_presentation/$', 'scalereg.simple_cfp.views.SubmitPresentation'),
 )
