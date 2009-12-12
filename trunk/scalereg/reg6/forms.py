ffbc94ab02d6ec18c2dfbf62e3ccf2e0f8c0e227
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-12-12 21:28:11 +0000

    reg6: Fix add coupon feature for Django 1.0.

--- trunk/scalereg/reg6/forms.py
+++ trunk/scalereg/reg6/forms.py
@@ -1,6 +1,22 @@
 from django.forms import ModelForm
 from scalereg.reg6.models import Attendee
+from scalereg.reg6.models import Order
 from scalereg.reg6.models import PromoCode
+
+class AddCouponForm(ModelForm):
+  class Meta:
+    model = Order
+    fields = (
+      'name',
+      'address',
+      'city',
+      'state',
+      'zip',
+      'email',
+      'country',
+      'phone',
+    )
+
 
 class AttendeeForm(ModelForm):
   class Meta:
