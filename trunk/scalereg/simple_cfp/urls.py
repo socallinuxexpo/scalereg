e37165453741c3eae2c94a72654c97ffca1cf958
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-12-23 09:09:33 +0000

    simple_cfp: Add a logout page.

--- trunk/scalereg/simple_cfp/urls.py
+++ trunk/scalereg/simple_cfp/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^$', 'scalereg.simple_cfp.views.index'),
+    (r'^logout/$', 'scalereg.simple_cfp.views.Logout'),
     (r'^recover_validation/$', 'scalereg.simple_cfp.views.RecoverValidation'),
     (r'^speaker_registration/$', 'scalereg.simple_cfp.views.RegisterSpeaker'),
     (r'^submission_status/$', 'scalereg.simple_cfp.views.SubmissionStatus'),
