425781b83501d80439058b19245b2103c006ac6f
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-12-30 08:52:47 +0000

    simple_cfp: Filter reviews by fields.

--- trunk/scalereg/simple_cfp/review/urls.py
+++ trunk/scalereg/simple_cfp/review/urls.py
@@ -2,4 +2,9 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^$', 'scalereg.simple_cfp.review.views.index'),
+    (r'^(?P<id>[0-9]+)/$', 'scalereg.simple_cfp.review.views.ReviewPresentation'),
+    (r'^audience/$', 'scalereg.simple_cfp.review.views.AudienceIndex'),
+    (r'^category/$', 'scalereg.simple_cfp.review.views.CategoryIndex'),
+    (r'^speaker/$', 'scalereg.simple_cfp.review.views.SpeakerIndex'),
+    (r'^status/$', 'scalereg.simple_cfp.review.views.StatusIndex'),
 )
