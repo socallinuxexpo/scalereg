67833b1cc51af7ec588598ced1e8c5d231b1904e
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2009-12-30 08:53:00 +0000

    simple_cfp: Filter reviews by a specific field instance.

--- trunk/scalereg/simple_cfp/review/views.py
+++ trunk/scalereg/simple_cfp/review/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseServerError
 from django.shortcuts import render_to_response
 from scalereg.simple_cfp import models
 
@@ -42,11 +43,37 @@ def AudienceIndex(request):
   TYPE = 'audience'
 
   audiences = models.Audience.objects.all()
-  audiences = [ obj.name for obj in audiences ]
+  audiences = [ (obj.id, obj.name) for obj in audiences ]
   return render_to_response('simple_cfp/review/review_by_type_index.html',
     {'title': TITLE,
      'type': TYPE,
      'list': audiences,
+    })
+
+
+@login_required
+def Audience(request, id=None):
+  TITLE = 'Presentations by Audience'
+
+  try:
+    audience = models.Audience.objects.get(id=id)
+  except models.Audience.DoesNotExist:
+    audience = None
+  if not audience:
+    return HttpResponseServerError('Unknown id')
+  presentations = models.Presentation.objects.filter(audiences=audience)
+  presentations = presentations.filter(valid=True)
+
+  my_reviews = models.Review.objects.filter(name=request.user)
+  my_reviews = [ r for r in my_reviews
+                 if audience in r.presentation.audiences.all() ]
+  (reviewed, unreviewed) = separate_unreviewed_presentations(my_reviews,
+                                                             presentations)
+
+  return render_to_response('simple_cfp/review/review_list.html',
+    {'title': TITLE + ': ' + audience.name,
+     'reviewed': reviewed,
+     'unreviewed': unreviewed,
     })
 
 
@@ -56,11 +83,37 @@ def CategoryIndex(request):
   TYPE = 'category'
 
   categories = models.Category.objects.all()
-  categories = [ obj.name for obj in categories ]
+  categories = [ (obj.id, obj.name) for obj in categories ]
   return render_to_response('simple_cfp/review/review_by_type_index.html',
     {'title': TITLE,
      'type': TYPE,
      'list': categories,
+    })
+
+
+@login_required
+def Category(request, id=None):
+  TITLE = 'Presentations by Category'
+
+  try:
+    category = models.Category.objects.get(id=id)
+  except models.Category.DoesNotExist:
+    category = None
+  if not category:
+    return HttpResponseServerError('Unknown id')
+  presentations = models.Presentation.objects.filter(categories=category)
+  presentations = presentations.filter(valid=True)
+
+  my_reviews = models.Review.objects.filter(name=request.user)
+  my_reviews = [ r for r in my_reviews
+                 if category in r.presentation.categories.all() ]
+  (reviewed, unreviewed) = separate_unreviewed_presentations(my_reviews,
+                                                             presentations)
+
+  return render_to_response('simple_cfp/review/review_list.html',
+    {'title': TITLE + ': ' + category.name,
+     'reviewed': reviewed,
+     'unreviewed': unreviewed,
     })
 
 
@@ -70,7 +123,8 @@ def SpeakerIndex(request):
   TYPE = 'speaker'
 
   speakers = models.Speaker.objects.all()
-  speakers = [ '%s %s' % (obj.first_name, obj.last_name) for obj in speakers ]
+  speakers = [ (obj.id, '%s %s' % (obj.first_name, obj.last_name))
+               for obj in speakers ]
   return render_to_response('simple_cfp/review/review_by_type_index.html',
     {'title': TITLE,
      'type': TYPE,
@@ -79,13 +133,61 @@ def SpeakerIndex(request):
 
 
 @login_required
+def Speaker(request, id=None):
+  TITLE = 'Presentations by Speaker'
+
+  try:
+    speaker = models.Speaker.objects.get(id=id)
+  except models.Speaker.DoesNotExist:
+    speaker = None
+  if not speaker:
+    return HttpResponseServerError('Unknown id')
+  presentations = models.Presentation.objects.filter(speaker=speaker)
+  presentations = presentations.filter(valid=True)
+
+  my_reviews = models.Review.objects.filter(name=request.user)
+  my_reviews = [ r for r in my_reviews if r.presentation.speaker == speaker ]
+  (reviewed, unreviewed) = separate_unreviewed_presentations(my_reviews,
+                                                             presentations)
+
+  return render_to_response('simple_cfp/review/review_list.html',
+    {'title': TITLE + ': ' + speaker.first_name + ' ' + speaker.last_name,
+     'reviewed': reviewed,
+     'unreviewed': unreviewed,
+    })
+
+
+@login_required
 def StatusIndex(request):
   TITLE = 'List of Status'
   TYPE = 'status'
 
-  status = [ m[1] for m in models.STATUS_CHOICES ]
+  status = models.STATUS_CHOICES
   return render_to_response('simple_cfp/review/review_by_type_index.html',
     {'title': TITLE,
      'type': TYPE,
      'list': status,
+    })
+
+
+@login_required
+def Status(request, status=None):
+  TITLE = 'Presentations by Speaker'
+
+  if status not in [ choice[0] for choice in models.STATUS_CHOICES ]:
+    status = None
+  if not status:
+    return HttpResponseServerError('Unknown status')
+  presentations = models.Presentation.objects.filter(status=status)
+  presentations = presentations.filter(valid=True)
+
+  my_reviews = models.Review.objects.filter(name=request.user)
+  my_reviews = [ r for r in my_reviews if r.presentation.status == status ]
+  (reviewed, unreviewed) = separate_unreviewed_presentations(my_reviews,
+                                                             presentations)
+
+  return render_to_response('simple_cfp/review/review_list.html',
+    {'title': TITLE + ': ' + status,
+     'reviewed': reviewed,
+     'unreviewed': unreviewed,
     })
