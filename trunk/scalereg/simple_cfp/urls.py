b4c85931a59640749ba0f10acf370244c326967e
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2010-01-02 20:29:03 +0000

    simple_cfp: Add extended RSS feed for accepted presentations and speakers.

--- trunk/scalereg/simple_cfp/urls.py
+++ trunk/scalereg/simple_cfp/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('',
     (r'^$', 'scalereg.simple_cfp.views.index'),
+    (r'^accepted_presentations/$', 'scalereg.simple_cfp.views.AcceptedPresentations'),
+    (r'^accepted_speakers/$', 'scalereg.simple_cfp.views.AcceptedSpeakers'),
     (r'^logout/$', 'scalereg.simple_cfp.views.Logout'),
     (r'^recover_validation/$', 'scalereg.simple_cfp.views.RecoverValidation'),
     (r'^speaker_registration/$', 'scalereg.simple_cfp.views.RegisterSpeaker'),
