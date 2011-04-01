ac4354163ce9b9dd2ac29991edff28b6528e0399
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2011-04-01 10:03:09 +0000

    reg6: Add a missing url for reprinting badges.

--- scalereg/reg6/staff/urls.py
+++ scalereg/reg6/staff/urls.py
@@ -5,4 +5,5 @@ urlpatterns = patterns('',
     (r'^checkin/$', 'scalereg.reg6.staff.views.CheckIn'),
     (r'^finish_checkin/$', 'scalereg.reg6.staff.views.FinishCheckIn'),
     (r'^cash_payment/$', 'scalereg.reg6.staff.views.CashPayment'),
+    (r'^reprint/$', 'scalereg.reg6.staff.views.Reprint'),
 )
