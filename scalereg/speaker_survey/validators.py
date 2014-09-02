ddd083d1cd776fa6711a159166b476b28cdd1baa
zhanglei <zhanglei@24f759cc-8348-0410-b45a-7b3cdd96a06e>
2014-09-02 07:37:03 +0000

    speaker_survey: Convert URLs to uppercase. Cleanup code.

--- scalereg/speaker_survey/validators.py
+++ scalereg/speaker_survey/validators.py
@@ -2,28 +2,32 @@ from django.conf import settings
 from scalereg.common.validators import ScaleValidationError
 from scalereg.reg6 import models
 import hashlib
+import string
+
+HASH_LENGTH = 6
 
 def hashfunc(data):
   data = settings.SCALEREG_SPEAKERSURVEY_SECRET + data.encode('utf-8')
-  return hashlib.sha1(data).hexdigest()
+  return hashlib.sha1(data).hexdigest().upper()
 
 def hashAttendee(attendee):
-  return hashfunc(attendee.first_name + attendee.last_name)[:6]
+  return hashfunc(attendee.first_name + attendee.last_name)[:HASH_LENGTH]
 
 def isValid7XHash(field_data, all_data):
   if not field_data:
     raise ScaleValidationError('Invalid hash object')
   if len(field_data) != 10:
     raise ScaleValidationError('Value must be exactly 10 digits')
-  for i in field_data[:6]:
-    if i not in '0123456789abcdef':
+  attendee_hash = field_data[:HASH_LENGTH].upper()
+  for i in attendee_hash:
+    if i not in string.hexdigits:
       raise ScaleValidationError('Invalid hash')
   try:
-    id = int(field_data[6:])
-    attendee = models.Attendee.objects.get(id=id)
+    attendee_id = int(field_data[HASH_LENGTH:])
+    attendee = models.Attendee.objects.get(id=attendee_id)
     if not attendee.valid or not attendee.checked_in:
       raise ScaleValidationError('Invalid attendee')
-    if field_data[:6] != hashAttendee(attendee):
+    if attendee_hash != hashAttendee(attendee):
       raise ScaleValidationError('Incorrect hash')
   except ValueError:
     raise ScaleValidationError('Not a number')
