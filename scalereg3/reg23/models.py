065e232c8b6f2cb9793e9eed629a3d91a30b5603
Lei Zhang <leizleiz@users.noreply.github.com>
2025-07-12 20:58:30 +0000

    reg23: Add Ticket model

--- scalereg3/reg23/models.py
+++ scalereg3/reg23/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+TICKET_CHOICES = (
+    ('exhibitor', 'Exhibitor'),
+    ('expo', 'Expo Only'),
+    ('full', 'Full'),
+    ('kid', 'Kids Pass'),
+    ('press', 'Press'),
+    ('speaker', 'Speaker'),
+    ('staff', 'Staff'),
+)
+
+
+class Ticket(models.Model):
+    name = models.CharField(
+        max_length=5,
+        primary_key=True,
+        help_text='Up to 5 letters, upper-case letters + numbers')
+    description = models.CharField(max_length=60)
+    ticket_type = models.CharField(max_length=10, choices=TICKET_CHOICES)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    public = models.BooleanField(
+        help_text='Publicly available on the order page')
+    cash = models.BooleanField(help_text='Available for cash purchase')
+    upgradable = models.BooleanField(help_text='Eligible for upgrades')
+    start_date = models.DateField(null=True,
+                                  blank=True,
+                                  help_text='Available on this day')
+    end_date = models.DateField(null=True,
+                                blank=True,
+                                help_text='Not Usable on this day')
