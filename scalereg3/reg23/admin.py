ba450729be563671f21143a67bbed8b6b27c7148
Lei Zhang <leizleiz@users.noreply.github.com>
2025-07-12 20:58:30 +0000

    reg23: Add Ticket model

--- scalereg3/reg23/admin.py
+++ scalereg3/reg23/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ticket
+
+
+class TicketAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'ticket_type', 'price', 'public',
+                    'cash', 'upgradable', 'start_date', 'end_date')
+    list_filter = ('ticket_type', 'public', 'start_date', 'end_date')
+    save_on_top = True
+
+
+admin.site.register(Ticket, TicketAdmin)
