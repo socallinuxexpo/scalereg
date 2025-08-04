9d3b8a9171c3080763d8d00a59820ad8328f1cd7
Lei Zhang <leizleiz@users.noreply.github.com>
2025-08-04 03:49:38 +0000

    reg23: Add Attendee and Order models

--- scalereg3/reg23/test_models.py
+++ scalereg3/reg23/test_models.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+from .models import Attendee
 from .models import Item
 from .models import Ticket
 
@@ -67,3 +68,49 @@ class TicketCostTest(TestCase):
     def test_with_regular_item_and_ticket_offset_item(self):
         cost = self.ticket.ticket_cost([self.item2, self.item3])
         self.assertEqual(cost, 20)
+
+
+class AttendeeFullNameTest(TestCase):
+
+    def test_name(self):
+        ticket = Ticket.objects.create(name='T1',
+                                       description='T1',
+                                       ticket_type='full',
+                                       price=10,
+                                       public=True,
+                                       cash=False,
+                                       upgradable=False)
+        attendee = Attendee.objects.create(badge_type=ticket,
+                                           first_name='Foo',
+                                           last_name='Bar Qux')
+        self.assertEqual(attendee.full_name(), 'Foo Bar Qux')
+
+
+class AttendeeTicketCostTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        ticket = Ticket.objects.create(name='T1',
+                                       description='T1',
+                                       ticket_type='full',
+                                       price=10,
+                                       public=True,
+                                       cash=False,
+                                       upgradable=False)
+        cls.item1 = Item.objects.create(name='I1',
+                                        description='Promo',
+                                        price=17,
+                                        active=True,
+                                        promo=False,
+                                        ticket_offset=False,
+                                        applies_to_all=True)
+        cls.attendee = Attendee.objects.create(badge_type=ticket,
+                                               first_name='Foo',
+                                               last_name='Bar')
+
+    def test_no_items(self):
+        self.assertEqual(self.attendee.ticket_cost(), 10)
+
+    def test_item_no_promo(self):
+        self.attendee.ordered_items.add(self.item1)
+        self.assertEqual(self.attendee.ticket_cost(), 27)
