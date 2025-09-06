38397017ed00479db19975074086cf2bfef87e1b
Lei Zhang <leizleiz@users.noreply.github.com>
2025-09-06 17:50:36 +0000

    sponsorship: Add Package and Item models

--- scalereg3/sponsorship/admin.py
+++ scalereg3/sponsorship/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Item
+from .models import Package
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'long_description', 'price',
+                    'active', 'promo', 'package_offset')
+    list_filter = ('active', 'promo', 'package_offset')
+    save_on_top = True
+
+
+class PackageAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'long_description', 'price',
+                    'public', 'start_date', 'end_date')
+    list_filter = ('public', 'start_date', 'end_date')
+    save_on_top = True
+
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Package, PackageAdmin)
