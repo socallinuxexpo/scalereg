8329fd3df12545cb4dc8469c1d4d43e262323c42
Lei Zhang <leizleiz@users.noreply.github.com>
2025-09-06 19:50:33 +0000

    sponsorship: Add Items pages

--- scalereg3/sponsorship/urls.py
+++ scalereg3/sponsorship/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('add_items/', views.add_items),
 ]
--- scalereg3/sponsorship/views.py
+++ scalereg3/sponsorship/views.py
@@ -1,8 +1,23 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
 
 from . import models
 
 STEPS_TOTAL = 5
+
+
+def check_vars(request, post):
+    if request.method != 'POST':
+        return redirect('/sponsorship/')
+
+    for var in post:
+        if var not in request.POST:
+            return render(
+                request, 'sponsorship_error.html', {
+                    'title': 'Registration Problem',
+                    'error_message': f'No {var} information.',
+                })
+    return None
 
 
 def index(request):
@@ -15,5 +30,33 @@ def index(request):
             'title': 'Sponsorship',
             'packages': avail_packages,
             'step': 1,
+            'steps_total': STEPS_TOTAL,
+        })
+
+
+def add_items(request):
+    required_vars = ['package']
+    r = check_vars(request, required_vars)
+    if r:
+        return r
+
+    package_name = request.POST['package']
+    try:
+        package = models.Package.public_objects.get(name=package_name)
+    except models.Package.DoesNotExist:
+        return render(
+            request, 'sponsorship_error.html', {
+                'title': 'Registration Problem',
+                'error_message': f'Package {package_name} not found.',
+            })
+
+    items = package.get_items()
+
+    return render(
+        request, 'sponsorship_items.html', {
+            'title': 'Sponsorship - Add Items',
+            'package': package,
+            'items': items,
+            'step': 2,
             'steps_total': STEPS_TOTAL,
         })
