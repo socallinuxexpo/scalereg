85c184ef58b8dc96ce9dfd4f79d123adfbe1a661
Lei Zhang <leizleiz@users.noreply.github.com>
2025-09-15 08:46:05 +0000

    reg23: Apply promos on start_payment and payment pages

--- scalereg3/reg23/test_models.py
+++ scalereg3/reg23/test_models.py
@@ -6,6 +6,7 @@ from .models import Answer
 from .models import Attendee
 from .models import Item
 from .models import PendingOrder
+from .models import PromoCode
 from .models import Question
 from .models import Ticket
 
@@ -21,6 +22,12 @@ class TicketCostTest(TestCase):
                                            public=True,
                                            cash=False,
                                            upgradable=False)
+        cls.promo = PromoCode.objects.create(
+            name='P1',
+            description='P1 all',
+            price_modifier=decimal.Decimal(0.6),
+            active=True,
+            applies_to_all=True)
         cls.item1 = Item.objects.create(name='I1',
                                         description='Item 1',
                                         price=decimal.Decimal(17),
@@ -32,7 +39,7 @@ class TicketCostTest(TestCase):
                                         description='Item 2',
                                         price=decimal.Decimal(6),
                                         active=True,
-                                        promo=False,
+                                        promo=True,
                                         ticket_offset=False,
                                         applies_to_all=True)
         cls.item3 = Item.objects.create(name='I3',
@@ -46,33 +53,45 @@ class TicketCostTest(TestCase):
                                         description='Ticket offset 1',
                                         price=decimal.Decimal(19),
                                         active=True,
-                                        promo=False,
+                                        promo=True,
                                         ticket_offset=True,
                                         applies_to_all=True)
 
     def test_no_items(self):
-        cost = self.ticket.ticket_cost([])
+        cost = self.ticket.ticket_cost([], None)
         self.assertEqual(cost, 10)
+        cost = self.ticket.ticket_cost([], self.promo)
+        self.assertEqual(cost, 6)
 
     def test_single_item(self):
-        cost = self.ticket.ticket_cost([self.item1])
+        cost = self.ticket.ticket_cost([self.item1], None)
         self.assertEqual(cost, 27)
+        cost = self.ticket.ticket_cost([self.item1], self.promo)
+        self.assertEqual(cost, 23)
 
     def test_multiple_items(self):
-        cost = self.ticket.ticket_cost([self.item1, self.item2])
+        cost = self.ticket.ticket_cost([self.item1, self.item2], None)
         self.assertEqual(cost, 33)
+        cost = self.ticket.ticket_cost([self.item1, self.item2], self.promo)
+        self.assertAlmostEqual(cost, decimal.Decimal(26.60))
 
     def test_with_ticket_offset_item(self):
-        cost = self.ticket.ticket_cost([self.item3])
+        cost = self.ticket.ticket_cost([self.item3], None)
+        self.assertEqual(cost, 14)
+        cost = self.ticket.ticket_cost([self.item3], self.promo)
         self.assertEqual(cost, 14)
 
     def test_with_two_ticket_offset_items(self):
-        cost = self.ticket.ticket_cost([self.item3, self.item4])
+        cost = self.ticket.ticket_cost([self.item3, self.item4], None)
         self.assertEqual(cost, 33)
+        cost = self.ticket.ticket_cost([self.item3, self.item4], self.promo)
+        self.assertAlmostEqual(cost, decimal.Decimal(25.40))
 
     def test_with_regular_item_and_ticket_offset_item(self):
-        cost = self.ticket.ticket_cost([self.item2, self.item3])
+        cost = self.ticket.ticket_cost([self.item2, self.item3], None)
         self.assertEqual(cost, 20)
+        cost = self.ticket.ticket_cost([self.item2, self.item3], self.promo)
+        self.assertAlmostEqual(cost, decimal.Decimal(17.60))
 
 
 class AttendeeCheckinCodeTest(TestCase):
@@ -119,7 +138,7 @@ class AttendeeTicketCostTest(TestCase):
                                        cash=False,
                                        upgradable=False)
         cls.item1 = Item.objects.create(name='I1',
-                                        description='Promo',
+                                        description='No promo item',
                                         price=decimal.Decimal(17),
                                         active=True,
                                         promo=False,
@@ -135,6 +154,66 @@ class AttendeeTicketCostTest(TestCase):
     def test_item_no_promo(self):
         self.attendee.ordered_items.add(self.item1)
         self.assertEqual(self.attendee.ticket_cost(), 27)
+
+
+class AttendeeTicketCostWithPromoTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        ticket = Ticket.objects.create(name='T1',
+                                       description='T1',
+                                       ticket_type='full',
+                                       price=decimal.Decimal(10),
+                                       public=True,
+                                       cash=False,
+                                       upgradable=False)
+        promo = PromoCode.objects.create(name='P1',
+                                         description='P1 all',
+                                         price_modifier=decimal.Decimal(0.6),
+                                         active=True,
+                                         applies_to_all=True)
+        cls.item1 = Item.objects.create(name='I1',
+                                        description='No promo item',
+                                        price=decimal.Decimal(17),
+                                        active=True,
+                                        promo=False,
+                                        ticket_offset=False,
+                                        applies_to_all=True)
+        cls.item2 = Item.objects.create(name='I2',
+                                        description='Promo item',
+                                        price=decimal.Decimal(30),
+                                        active=True,
+                                        promo=True,
+                                        ticket_offset=False,
+                                        applies_to_all=True)
+        cls.attendee = Attendee.objects.create(badge_type=ticket,
+                                               first_name='Foo',
+                                               last_name='Bar',
+                                               promo=promo)
+
+    def test_no_items(self):
+        self.assertEqual(self.attendee.ticket_cost(), 6)
+        # Make sure there are no side effects in the previous call.
+        self.assertEqual(self.attendee.ticket_cost(), 6)
+
+    def test_item_no_promo(self):
+        self.attendee.ordered_items.add(self.item1)
+        self.assertEqual(self.attendee.ticket_cost(), 23)
+        # Make sure there are no side effects in the previous call.
+        self.assertEqual(self.attendee.ticket_cost(), 23)
+
+    def test_item_promo(self):
+        self.attendee.ordered_items.add(self.item2)
+        self.assertEqual(self.attendee.ticket_cost(), 24)
+        # Make sure there are no side effects in the previous call.
+        self.assertEqual(self.attendee.ticket_cost(), 24)
+
+    def test_items(self):
+        self.attendee.ordered_items.add(self.item1)
+        self.attendee.ordered_items.add(self.item2)
+        self.assertEqual(self.attendee.ticket_cost(), 41)
+        # Make sure there are no side effects in the previous call.
+        self.assertEqual(self.attendee.ticket_cost(), 41)
 
 
 class PendingOrderTest(TestCase):
