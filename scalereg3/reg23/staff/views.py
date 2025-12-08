2b85980ebb51abe963198eed10bb74ecd78de7e4
Lei Zhang <leizleiz@users.noreply.github.com>
2025-12-08 06:30:29 +0000

    reg23: Add staff receipt page

---
+++ scalereg3/reg23/staff/views.py
@@ -0,0 +1,25 @@
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import render
+
+from reg23 import views as reg23_views
+
+
+@staff_member_required
+def receipt(request):
+    attendee = None
+    error_message = None
+    if request.method == 'POST' and 'attendee' in request.POST:
+        attendee = reg23_views.get_attendee_for_id(request.POST['attendee'])
+        if attendee:
+            if (not attendee.valid or not attendee.order
+                    or not attendee.order.valid):
+                attendee = None
+                error_message = 'Invalid attendee'
+        else:
+            error_message = 'Attendee not found'
+    return render(
+        request, 'reg_staff_receipt.html', {
+            'title': 'Receipt lookup',
+            'attendee': attendee,
+            'error_message': error_message,
+        })
