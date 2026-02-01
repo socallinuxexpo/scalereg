8d0257cfb5894c4d24d2c570067ff28984f8e2b1
Lei Zhang <leizleiz@users.noreply.github.com>
2026-02-01 23:52:13 +0000

    reg23: Refactor reg23/test_models.py

--- scalereg3/reg23/test_models.py
+++ scalereg3/reg23/test_models.py
@@ -16,27 +16,35 @@ from .models import Upgrade
 
 class ModelStrTest(TestCase):
 
+    @classmethod
+    def setUpTestData(cls):
+        cls.ticket = Ticket.objects.create(name='T1',
+                                           description='Ticket 1',
+                                           ticket_type='full',
+                                           price=decimal.Decimal(10),
+                                           public=True,
+                                           cash=False,
+                                           upgradable=False)
+        cls.order = Order.objects.create(order_num='ORDER12345',
+                                         name='Order 1',
+                                         address='Addr',
+                                         city='City',
+                                         state='State',
+                                         zip_code='12345',
+                                         email='test@example.com',
+                                         amount=decimal.Decimal(10),
+                                         payment_type='payflow')
+        cls.attendee = Attendee.objects.create(badge_type=cls.ticket,
+                                               first_name='Foo',
+                                               last_name='Bar',
+                                               email='test@example.com',
+                                               zip_code='12345')
+
     def test_order_str(self):
-        order = Order.objects.create(order_num='ORDER1',
-                                     name='Order 1',
-                                     address='Addr',
-                                     city='City',
-                                     state='State',
-                                     zip_code='12345',
-                                     email='test@example.com',
-                                     amount=decimal.Decimal(10),
-                                     payment_type='payflow')
-        self.assertEqual(str(order), 'ORDER1')
+        self.assertEqual(str(self.order), 'ORDER12345')
 
     def test_ticket_str(self):
-        ticket = Ticket.objects.create(name='T1',
-                                       description='Ticket 1',
-                                       ticket_type='full',
-                                       price=decimal.Decimal(10),
-                                       public=True,
-                                       cash=False,
-                                       upgradable=False)
-        self.assertEqual(str(ticket), 'T1')
+        self.assertEqual(str(self.ticket), 'T1')
 
     def test_promo_code_str(self):
         promo = PromoCode.objects.create(name='P1',
@@ -55,74 +63,25 @@ class ModelStrTest(TestCase):
         self.assertEqual(str(item), 'Item 1 (I1)')
 
     def test_attendee_str(self):
-        ticket = Ticket.objects.create(name='T1',
-                                       description='Ticket 1',
-                                       ticket_type='full',
-                                       price=decimal.Decimal(10),
-                                       public=True,
-                                       cash=False,
-                                       upgradable=False)
-        attendee = Attendee.objects.create(badge_type=ticket,
-                                           first_name='Foo',
-                                           last_name='Bar',
-                                           email='test@example.com',
-                                           zip_code='12345')
-        self.assertEqual(str(attendee), f'({attendee.id}) (test@example.com)')
+        self.assertEqual(str(self.attendee), '(1) (test@example.com)')
 
     def test_pending_order_str(self):
         po = PendingOrder.objects.create(order_num='PEND1')
         self.assertEqual(str(po), 'PEND1')
 
     def test_payment_code_str(self):
-        ticket = Ticket.objects.create(name='T1',
-                                       description='Ticket 1',
-                                       ticket_type='full',
-                                       price=decimal.Decimal(10),
-                                       public=True,
-                                       cash=False,
-                                       upgradable=False)
-        order = Order.objects.create(order_num='ORDER1',
-                                     name='Order 1',
-                                     address='Addr',
-                                     city='City',
-                                     state='State',
-                                     zip_code='12345',
-                                     email='test@example.com',
-                                     amount=decimal.Decimal(10),
-                                     payment_type='payflow')
         pc = PaymentCode.objects.create(code='CODE1',
-                                        badge_type=ticket,
-                                        order=order,
+                                        badge_type=self.ticket,
+                                        order=self.order,
                                         max_attendees=1)
         self.assertEqual(str(pc), 'CODE1')
 
     def test_upgrade_str(self):
-        ticket = Ticket.objects.create(name='T1',
-                                       description='Ticket 1',
-                                       ticket_type='full',
-                                       price=decimal.Decimal(10),
-                                       public=True,
-                                       cash=False,
-                                       upgradable=False)
-        order = Order.objects.create(order_num='ORDER1',
-                                     name='Order 1',
-                                     address='Addr',
-                                     city='City',
-                                     state='State',
-                                     zip_code='12345',
-                                     email='test@example.com',
-                                     amount=decimal.Decimal(10),
-                                     payment_type='payflow')
-        attendee = Attendee.objects.create(badge_type=ticket,
-                                           first_name='Foo',
-                                           last_name='Bar',
-                                           email='test@example.com',
-                                           zip_code='12345')
-        upgrade = Upgrade.objects.create(attendee=attendee,
-                                         old_badge_type=ticket,
-                                         old_order=order,
-                                         new_badge_type=ticket)
-        self.assertEqual(str(upgrade), str(attendee))
+        upgrade = Upgrade.objects.create(attendee=self.attendee,
+                                         old_badge_type=self.ticket,
+                                         old_order=self.order,
+                                         new_badge_type=self.ticket)
+        self.assertEqual(str(upgrade), '(1) (test@example.com)')
 
 
 class TicketCostTest(TestCase):
